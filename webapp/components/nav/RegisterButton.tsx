d5db07649c6d25e87c32d79f01451f6d9d977698
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2025-10-21 15:28:36 -0300

    fix: register btn url.

--- webapp/components/nav/RegisterButton.tsx
+++ webapp/components/nav/RegisterButton.tsx
@@ -7,7 +7,7 @@ export default function RegisterButton() {
   const redirectUrl = process.env.AUTH_URL;
 
   const registerUrl = `${issuer}/protocol/openid-connect/registrations` +
-      `?client_id=${clientId}$redirect_uri=` +
+      `?client_id=${clientId}&redirect_uri=` +
       `${encodeURIComponent(redirectUrl!)}&response_type=code&scope=openid`;
   return (
     <Button as='a' href={registerUrl} color='secondary'>Register</Button>
